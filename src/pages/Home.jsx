5486741442aab8a04f7c36d8b4693c2da498d63a
Sp3n99 <srasor10@gmail.com>
2022-08-08 17:35:41 -0500

    added components folder

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./home.scss"
+import Topbar from "../components/Topbar"
+
+const Home = () => {
+  return (
+    <div className="home">
+        <Topbar />
+    </div>
+  )
+}
+
+export default Home
