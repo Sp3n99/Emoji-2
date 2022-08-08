5cd17eb9e14a6ed01b08d9b8c12183958bac1657
Sp3n99 <srasor10@gmail.com>
2022-08-08 18:29:44 -0500

    emoji selector
    began implementing emoji selector

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,24 @@ const Home = () => {
   return (
     <div className="home">
         <Topbar />
+        <div className = "wrapper">
+            <div className="image-container">
+                <img src="https://www.dictionary.com/e/wp-content/uploads/2018/03/Thinking_Face_Emoji-Emoji-Island.png" />
+                <form>
+                    <input type="text" placeholder="Name" />
+                </form>
+            </div>
+            <div className="options-container">
+                <div className="options-wrapper">
+                    <div className="option">
+                        <h4>Eyes:</h4>
+                        <div className="image-selector">
+                            <img src="https://www.dictionary.com/e/wp-content/uploads/2018/03/Thinking_Face_Emoji-Emoji-Island.png" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
