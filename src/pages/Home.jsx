d8ad99fa1a983e4ab3087349d93e61f10705cfdc
Connor Wilson <connorwilson989@gmail.com>
2022-08-09 09:30:44 -0500

    start of the image merge logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import "./home.scss"
 import Topbar from "../components/Topbar"
+import mergeImages from 'merge-images-v2'
+import base from "../images/base.png"
+import eyes from "../images/eyes.png"
+import mouth from "../images/mouth.png"
 
-const Home = () => {
+export default function Home() {
+  const [src, setSrc] = useState(null);
+
+  useEffect(() => {
+    mergeImages([base])
+      .then((src) => setSrc(src))
+      .catch((err) => console.log(err));
+  }, []);
+
+  function handleClick(selection) {
+    mergeImages([base, selection])
+      .then((src) => setSrc(src))
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="home">
         <Topbar />
         <div className = "wrapper">
             <div className="image-container">
-                <img src="https://www.dictionary.com/e/wp-content/uploads/2018/03/Thinking_Face_Emoji-Emoji-Island.png" />
+                <img src={src} alt="emoji preview" />
                 <form>
-                    <input type="text" placeholder="Name" />
+                    <input type="text" placeholder="Name"/>
                 </form>
             </div>
             <div className="options-container">
                 <div className="options-wrapper">
-                    <div className="option">
+                    <div className="option" onClick={() => { handleClick(eyes) }}>
                         <h4>Eyes:</h4>
                         <div className="image-selector">
-                            <img src="https://www.dictionary.com/e/wp-content/uploads/2018/03/Thinking_Face_Emoji-Emoji-Island.png" />
+                            <img src={eyes} alt="emoji eyes selector" />
+                        </div>
+                    </div>
+                    <div className="option" onClick={() => { handleClick(mouth) }}>
+                        <h4>Mouth:</h4>
+                        <div className="image-selector">
+                            <img src={mouth} alt="emoji eyes selector" />
                         </div>
                     </div>
                 </div>
@@ -27,5 +50,3 @@ const Home = () => {
     </div>
   )
 }
-
-export default Home
